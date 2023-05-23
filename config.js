6df07e520d8125950261325dbb6273285d53421c
Rahsidali <134333196+Rahsidali@users.noreply.github.com>
2023-05-24 02:15:37 +0500

    Update config.js

--- config.js
+++ config.js
@@ -21,25 +21,25 @@ global.lApiKey = [ // if this API key get expired then please go to https://api.
 ]
 
 
-global.botname = "Queen Anya" 
-global.author = "𝙋𝙄𝙆𝘼"  
-global.packname = "𝙌𝙐𝙀𝙀𝙉✯𝘼𝙉𝙔𝘼✯𝘽𝙊𝙏"  
+global.botname = "IMTIAZ SINDHI" 
+global.author = "Ali"  
+global.packname = "IMTIAZ✯𝘼li✯𝘽𝙊𝙏"  
 global.myweb = "https://github.com/PikaBotz" // You can put any link 
-global.waterMark = "×͜× King ✘ Pika™ ✓" // Your own watermark 
-global.footer = "© Queen Anya Bot"
+global.waterMark = "×͜× King ✘ Sindh™ ✓" // Your own watermark 
+global.footer = "© Imtiaz ali Bot"
 global.prefa = ['-'] // single prefix
 global.themeemoji = '🔥'
 global.socialText = `*⪧⪢ Please join this group for by bot's announcement.*`
 global.socialLink = `*⪧⪢ https://chat.whatsapp.com/HshHS6kHF9NHnA9lfMwwSM 💝*`
 
 
-global.ownername = "𝕻𝖎𝖐𝖆♰𝕶𝖚𝖓"   // Owner name //        
-global.ownernumber = ['918811074852'] 
+global.ownername = "IMTIAZ SINDHI"   // Owner name //        
+global.ownernumber = ['923111396960'] 
 global.socialm = "𝗜𝗻𝘀𝘁𝗮 : 3.69_pika" // 𝗜𝗻𝘀𝘁𝗮 : || 𝗚𝗶𝘁𝗵𝘂𝗯 : || 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸 : \\
 global.ytname = "YT: Ytname"   // Yt chanel name //
 global.continent = "Asia" // your continent name 
-global.region = "India" // your country name
-global.state = "Assam" // your state name
+global.region = "Pakistan" // your country name
+global.state = "Sindh" // your state name
 global.district = "Dibrugarh" // your district name
 global.timezone = "Asia/Kolkata" // search on google if you don't know the timezone of your country //
 global.instagramId = "3.69_pika" // Your insta ID
